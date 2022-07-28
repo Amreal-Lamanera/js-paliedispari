118eaabcf3c2e51b431285c5a157dde87aadf309
Amreal-Lamanera <pieraccini.francesco@gmail.com>
2022-07-28 14:56:48 +0200

    add content even or odd

--- script.js
+++ script.js
@@ -1,25 +1,68 @@
-// chiedo all'utente una parola
-const urWord = prompt("Inserisci una parola o una frase palindroma");
-let result = "Parola/frase non palindroma!"
+// chiedo all'utente a quale gioco vuole giocare
+const wichGame = parseInt(prompt("Inserisci 1 per il gioco sulle parole palindrome, 2 per pari e dispari"));
 
-if (urWord === null) {
-    console.warn("Non hai inserito una parola - ricarica la pagina per riprovare");
-} else {
-    // creo un array di lettere dalla parola
-    let myArray = urWord.split("");
-    console.log(myArray);
+if (wichGame === 1) {
 
-    // rimuovo gli spazi (se presenti)
-    myArray = myArray.filter(element => {
-        return element !== " ";
-    });
-    console.log(myArray);
+    // chiedo all'utente una parola
+    const urWord = prompt("Inserisci una parola o una frase palindroma");
+    let result = "Parola/frase non palindroma!"
 
-    // chiamo funzione che controlla la palindromia
-    if (isPalindrome(myArray)) {
-        result = "Parola/frase palindroma!";
+    if (urWord === null) {
+        console.warn("Non hai inserito una parola - ricarica la pagina per riprovare");
+    } else {
+        // creo un array di lettere dalla parola
+        let myArray = urWord.split("");
+        // console.log(myArray);
+
+        // rimuovo gli spazi (se presenti)
+        myArray = myArray.filter(element => {
+            return element !== " ";
+        });
+        // console.log(myArray);
+
+        // chiamo funzione che controlla la palindromia
+        if (isPalindrome(myArray)) {
+            result = "Parola/frase palindroma!";
+        }
+        console.log(result);
+
     }
-    console.log(result);
+} else if (wichGame === 2) {
+
+    const evenOdd = prompt('Scegli "pari" o "dispari" scrivendolo qua sotto');
+    // console.log(evenOdd);
+    if (evenOdd !== "pari" && evenOdd !== "Pari" && evenOdd !== "dispari" && evenOdd !== "Dispari") {
+        console.warn("Scelta non valida - ricarica la pagina per riprovare");
+    } else {
+        let urChoice = false;
+        if (evenOdd === "pari" || evenOdd === "Pari") {
+            urChoice = true;
+        }
+        // utente sceglie un numero
+        const urNum = parseInt(prompt("Inserisci un numero fra 1 e 5"));
+        if (!isNaN(urNum)) {
+            if (urNum > 0 && urNum < 5) {
+                // genero un numero casuale per il pc
+                const myNum = getRandomIntBetween(1, 5);
+                console.log("IL TUO NUMERO: ", urNum, "IL MIO NUMERO: ", myNum);
+                const sum = myNum + urNum;
+                let result = "Hai perso!";
+                // Utilizzo uno XOR negato (vero se entrambe vere o entrambe false)
+                // => se è pari e hai scelto pari
+                // => se è dispari e hai scelto dispari
+                // con pari = true
+                if (!(isEven(sum) ^ urChoice)) {
+                    result = "Hai vinto!";
+                }
+                console.log(result);
+            }
+        } else {
+            console.warn("Numero inserito non corretto - ricarica la pagina per riprovare");
+        }
+    }
+
+} else {
+    alert("Valore non corretto - ricarica la pagina per riprovare")
 }
 
 /***********************
@@ -35,6 +78,7 @@ function invertArray(list) {
     return newArray
 }
 
+// funzione che restituisce true se l'array è palindromo, false altrimenti
 function isPalindrome(list) {
     // creo un array identico, ma invertito
     const myArrayReverse = invertArray(list);
@@ -51,4 +95,27 @@ function isPalindrome(list) {
 
     // Trasformo gli array in stringhe e controllo se sono uguali, ritorno il valore del confronto
     return myArrayReverse.join(',') === list.join(',')
+}
+
+// funzione che genera un numero casuale da min a max
+function getRandomIntBetween(min, max) {
+    const n = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (isNaN(min) || isNaN(max)) {
+        console.warn("ERRORE - hai inserito dei dati non numerici");
+        return;
+    }
+    if (min > max) {
+        console.warn("ERRORE - minimo è più grande di massimo");
+        return;
+    }
+    return n;
+}
+
+// funzione che restituisce true se il numero è pari, false altrimenti
+function isEven(num) {
+    if (typeof num !== 'number' || isNaN(num)) {
+        console.warn('not a number');
+        return;
+    }
+    return num % 2 === 0;
 }
